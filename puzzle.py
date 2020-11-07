9e087e83501dc819a0392ce90e02498d5b3737ba
Matthew Wills <mcwills20@gmail.com>
2020-11-07 11:21:02 -0500

    Refactored build_puzzle to use more inplace operations and limiting the number of new lists being initialized.

--- puzzle.py
+++ puzzle.py
@@ -13,10 +13,9 @@ class box(object):
         # Initialize the possible dict
         self.initpossible()
 
-    # When printing, display the value    
+    # When printing, display the value
     def __repr__(self):
         return str(self.value)
-
 
     def getquad(self):
 
@@ -51,34 +50,30 @@ class box(object):
             else:
                 return 8
 
-
     def initpossible(self):
         self.possible = dict()
         if self.value == 0:
-            for i in range(1,10):
+            for i in range(1, 10):
                 self.possible[i] = True
         else:
-            for i in range(1,10):
+            for i in range(1, 10):
                 self.possible[i] = False
 
             self.possible[self.value] = True
 
+
 def build_puzzle(raw):
     # cast the raw data (which should be a string of numbers with no delimination) into a list for easier sorting
-    _lis = list(str(raw))
+    _lis = list(raw)
     # initialize a temporary formatted list. 9 Lists (the rows) of 9 numbers each will be put into this list
     _formattedlis = []
-    for i in range(0,81,9):
+    for i in range(0, 81, 9):
         _formattedlis.append(_lis[i:i+9])
 
-    # use the formatted puzzle to create a list of box objects
-    _finallis = []
+    # covert the lists from the raw values into the box object types
     for rownum, row in enumerate(_formattedlis):
-        _row = []
         for colnum, val in enumerate(row):
-            _row.append(box(int(val), rownum, colnum))
-        _finallis.append(_row)
-    
-    # use the final list to return a pandas dataframe with the box objects
+            _formattedlis[rownum][colnum] = box(int(val), rownum, colnum)
 
-    return pd.DataFrame(_finallis)
+    # use the final list to return a pandas dataframe with the box objects
+    return pd.DataFrame(_formattedlis)
