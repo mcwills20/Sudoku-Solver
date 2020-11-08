08b32f1bd606f08c08d269e989425d98874ee62f
Matthew Wills <mcwills20@gmail.com>
2020-11-08 14:22:44 -0500

    Added intermediate column check Addeding intermediate quadrant check and refactoring simple check_quad

--- solve.py
+++ solve.py
@@ -66,21 +66,9 @@ def check_quad(quad, sudoku):
 
     return sudoku
 
-
-def check_values(df):
-
-    # Loop through the loop to see which
-    found = []
-    for box in df:
-        if box.value != 0:
-            found.append(box.value)
-
-    return found
-
-
 # Intermediate checks will check the possible values of the boxes in a region
 # If only one box in a region has a specfic possible value, that can be considered solved
-def intermediate_check_row(rownum, sudoku):
+def int_check_row(rownum, sudoku):
 
     # Perform a basic check to update possible values
     _found = check_values(sudoku.loc[rownum])
@@ -110,3 +98,51 @@ def intermediate_check_row(rownum, sudoku):
                 box.check_solved()
 
     return sudoku
+
+def int_check_column(colnum, sudoku):
+    
+    # Perform a basic check to update possible values
+    _found = check_values(sudoku.loc[:,colnum])
+
+    for box in sudoku.loc[:,colnum]:
+        if not box.solved:
+            # Create a temporary list of possible values to check for
+            _possible = box.possible.copy()
+            # Remove any found values from temporary list
+            for val in _found:
+                if val in _possible:
+                    _possible.remove(val)
+
+            # Loop through the other boxes in the row to find their possible values
+            for checkbox in sudoku.loc[:,colnum]:
+                # Only check if the box isn't solved and it isn't the box we are comparing to
+                if not checkbox.solved and box != checkbox:
+                    # Loop through all the possible values in the check box
+                    for _pos in checkbox.possible:
+                        # If we find a value that is in the source, remove it
+                        if _pos in _possible:
+                            _possible.remove(_pos)
+
+            # If there is only one possible solution left, that must be the solution for this box.
+            if len(_possible) == 1:
+                box.possible = _possible
+                box.check_solved()
+
+    return sudoku
+
+
+    
+# Function to check the solved values in a region
+def check_values(df):
+    # Loop through the loop to see which
+    found = []
+    for box in df:
+        if box.value != 0:
+            found.append(box.value)
+
+    return found
+
+# Function to slice the Sudoku Puzzle into a quadrant
+def get_quad(quad, df):
+    pass
+
