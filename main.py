5df09a51fffc113b1bc6aa6756a43b6b7a5ebdc5
Matthew Wills <mcwills20@gmail.com>
2020-11-23 09:29:01 -0500

    Refactored the update_possible function on the GUI

--- main.py
+++ main.py
@@ -45,54 +45,7 @@ class SudokuCell(ButtonBehavior, GridLayout):
                 self.ids[labelid].text = str(position)
             else:
                 self.ids[labelid].text = ''
-        
-            
-        """
-        if 1 in self.possible:
-            self.ids.pos1.text = '1'
-        else:
-            self.ids.pos1.text = ''
 
-        if 2 in self.possible:
-            self.ids.pos2.text = '2'
-        else:
-            self.ids.pos2.text = ''
-
-        if 3 in self.possible:
-            self.ids.pos3.text = '3'
-        else:
-            self.ids.pos3.text = ''
-
-        if 4 in self.possible:
-            self.ids.pos4.text = '4'
-        else:
-            self.ids.pos4.text = ''
-
-        if 5 in self.possible:
-            self.ids.pos5.text = '5'
-        else:
-            self.ids.pos5.text = ''
-
-        if 6 in self.possible:
-            self.ids.pos6.text = '6'
-        else:
-            self.ids.pos6.text = ''
-
-        if 7 in self.possible:
-            self.ids.pos7.text = '7'
-        else:
-            self.ids.pos7.text = ''
-
-        if 8 in self.possible:
-            self.ids.pos8.text = '8'
-        else:
-            self.ids.pos8.text = ''
-
-        if 9 in self.possible:
-            self.ids.pos9.text = '9'
-        else:
-            self.ids.pos9.text = ''
-"""
     def update_solution(self, value):
         self.possible = []
         self.update_possible(self.possible)
@@ -100,7 +53,6 @@ class SudokuCell(ButtonBehavior, GridLayout):
         self.ids.pos5.font_size = 25
         self.ids.pos5.color = [0, 0, 0, 1]
         self.color = [0, 1, 0, 1]
-    
 
 
 class QuadrantGrid(GridLayout):
@@ -116,7 +68,6 @@ class SudokuGrid(GridLayout):
 class ManualInput(BoxLayout):
     def __init__(self, **kwargs):
         super(ManualInput, self).__init__(**kwargs)
-
 
 
 class SudokuPy(App):
@@ -137,8 +88,7 @@ class SudokuPy(App):
         # Build main puzzle interface
 
         interface = BoxLayout(orientation='horizontal', size_hint=(1, .7))
-      
-               
+
         # Build the sudoku grid and add it to the interface
         puzzle = SudokuGrid(size_hint=(3.5, 1))
         for quad in range(9):
@@ -216,54 +166,52 @@ class SudokuPy(App):
 
     def test(self, event):
         pass
-        
 
     def change_entry(self, button):
         for but in button.parent.children:
-            but.background_color = [1,1,1,1]
-        
+            but.background_color = [1, 1, 1, 1]
+
         self.entry = int(button.text)
-        button.background_color = [0,1,0,1]
+        button.background_color = [0, 1, 0, 1]
 
     def change_entry_type(self, button):
-        
-        button.background_color = [0,1,0,1]
+
+        button.background_color = [0, 1, 0, 1]
         # Check which button is being press
         if button.text == 'Pen':
             self.pen = True
             self.pencil = False
             # Clear color formatting on pencil button
-            button.parent.children[0].background_color = [1,1,1,1]
-            
+            button.parent.children[0].background_color = [1, 1, 1, 1]
+
         else:
             self.pencil = True
             self.pen = False
             # Clear color formatting on pen button
-            button.parent.children[1].background_color = [1,1,1,1]
+            button.parent.children[1].background_color = [1, 1, 1, 1]
 
     def manual_entry(self, cell):
         if self.entry != '':
             if self.pen:
                 # Assign the solution to the backend sudoku puzzle, not just the GUI element
                 cell.cell.assign_solution(int(self.entry))
-                cell.color = [1,1,1,1]
+                cell.color = [1, 1, 1, 1]
             elif self.pencil:
                 # Unlike with the pen, the pencil modifies a possible list that is only on the GUI element. It does not touch the backend
                 if self.entry in cell.man_possible:
                     cell.man_possible.remove(int(self.entry))
-                    
+
                 else:
                     cell.man_possible.add(int(self.entry))
 
                 cell.update_possible(cell.man_possible)
 
 
-
-
 sudoku = puzzle.build_sudoku(
     "200000001003060008807031940002506070409800056100000380038670500705090263000004000")
 
-blank = puzzle.build_sudoku("000000000000000000000000000000000000000000000000000000000000000000000000000000000")
+blank = puzzle.build_sudoku(
+    "000000000000000000000000000000000000000000000000000000000000000000000000000000000")
 
 if __name__ == '__main__':
     SudokuPy(sudoku).run()
