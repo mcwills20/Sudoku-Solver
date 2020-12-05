e5e5e376691649b8d8166818b04e5d9fbd698ca6
Matthew Wills <mcwills20@gmail.com>
2020-12-05 15:13:28 -0500

    Removed unnecessary imports

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ import solve
 import solve_utils as utils
 import backtrack
 
-import time
-
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.properties import StringProperty, ListProperty
